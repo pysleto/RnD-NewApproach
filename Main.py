92f4aba022405e688eb5f9e46087040dae3f9704
Simon Letout <pysleto@gmail.com>
2020-02-28 19:28:54 +0100

    Format and refactor code

--- Main.py
+++ Main.py
@@ -2,6 +2,10 @@
 import pandas as pd
 import Input as input
 
+# <editor-fold desc="STEP #0 - Initialisation">
+
+print('STEP #0 - Initialisation')
+
 # Set configuration parameters
 case = 'EU_28'
 base = r'C:\Users\letousi\PyProjs\RnD-NewApproach'
@@ -17,6 +21,11 @@ if not file_path.exists():
 
 print('Read country mapping table ...')
 CountryMap = pd.read_csv(config['CASE_ROOT'].joinpath(r'Mapping\Country_table.csv'))
+# </editor-fold>
+
+# <editor-fold desc="STEP #1 - Selection of main companies">
+
+print('STEP #1 - Selection of main companies')
 
 # Select main companies by world region
 file_path = config['CASE_ROOT'].joinpath(r'Listed companies.csv')
@@ -25,7 +34,18 @@ if not file_path.exists():
     input.select_main(config['CASE_ROOT'], config['YEAR_LASTAV'], config['REGION'], CountryMap)
 
 print('Read list of selected listed companies ...')
-SelectMain = pd.read_csv(config['CASE_ROOT'].joinpath(r'Listed companies.csv'))
+SelectMain = pd.read_csv(
+    config['CASE_ROOT'].joinpath(r'Listed companies.csv'),
+    na_values='n.a.',
+    dtype={
+        col: str for col in ['BvD9', 'BvD_id']
+    }
+)
+# </editor-fold>
+
+# <editor-fold desc="STEP #2 - Consolidation of subsidiaries and main companies">
+
+print('STEP #2 - Consolidation of subsidiaries and main companies')
 
 # Load list of subsidiaries
 file_path = config['CASE_ROOT'].joinpath(r'Listed companies subsidiaries.csv')
@@ -34,4 +54,11 @@ if not file_path.exists():
     input.select_subs(config['CASE_ROOT'],config['SUBS_ID_FILE_N'])
 
 print('Read list of corresponding subsidiaries ...')
-SelectSubs = pd.read_csv(config['CASE_ROOT'].joinpath(r'Listed companies subsidiaries.csv'))
+SelectSubs = pd.read_csv(
+    config['CASE_ROOT'].joinpath(r'Listed companies subsidiaries.csv'),
+    na_values='n.a.',
+    dtype={
+        col: str for col in ['BvD9', 'BvD_id', 'Sub_BvD9', 'Sub_BvD_id']
+    }
+)
+# </editor-fold>
